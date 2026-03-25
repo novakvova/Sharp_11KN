af4086b44d4f7c82198f6f344799998ffe72473f
novakvova <novakvova@gmail.com>
2026-03-25 12:18:59 +0200

    Working email

---
+++ Projects/Lesson 13.ListView/MyListView/MyListView/MainForm.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace MyListView
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            listView1 = new ListView();
+            textBox1 = new TextBox();
+            label1 = new Label();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // listView1
+            // 
+            listView1.Location = new Point(48, 114);
+            listView1.Name = "listView1";
+            listView1.Size = new Size(676, 308);
+            listView1.TabIndex = 0;
+            listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(48, 53);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(544, 31);
+            textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(48, 19);
+            label1.Name = "label1";
+            label1.Size = new Size(134, 25);
+            label1.TabIndex = 2;
+            label1.Text = "Шлях до папки";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(598, 19);
+            button1.Name = "button1";
+            button1.Size = new Size(132, 78);
+            button1.TabIndex = 3;
+            button1.Text = "Показати вміст";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button1);
+            Controls.Add(label1);
+            Controls.Add(textBox1);
+            Controls.Add(listView1);
+            Name = "MainForm";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private ListView listView1;
+        private TextBox textBox1;
+        private Label label1;
+        private Button button1;
+    }
+}
