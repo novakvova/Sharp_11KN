005e51d2f810401b56fa8e8a816a9d14fb5d9389
novakvova <novakvova@gmail.com>
2026-05-29 12:57:15 +0300

    Add View Quersions

---
+++ Projects/Lesson 21.PDR/WinAppPDR/WinAppPDR/QuestionData.cs
@@ -0,0 +1,9 @@
+﻿public struct QuestionData
+{
+    public string Text;
+    public string ImagePath;
+    public string[] Options; // Масив варіантів відповідей
+    public int CorrectIndex;  // Індекс правильної відповіді (0, 1 або 2)
+    public Color ButtonColor; // Зберігаємо стан кнопки тут
+    public int? SelectedOption; // Який варіант обрав користувач
+}
